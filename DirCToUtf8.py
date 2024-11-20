2964ccf4ba9a42eda448c08dc702bf93280c9384
Lizzos Gabriele <gabriele.lizzos@atom.it>
2024-11-20 11:13:07 +0100

    created DirCToUtf8.py

---
+++ DirCToUtf8.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+import rich
+
+from common import convert_and_check_utf8
+from util.log import format_log_error
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, required=True, help='Path of the file to convert.')
+    parser.add_argument('--checks', action='store_true', help='Enable checks for the utf8 file')
+
+    args = parser.parse_args()
+
+    # Chiedi conferma all'utente
+    confirm = input(f"Your about to replace current encoding to (UTF-8 with BOM) in: {args.path}\nProceed? (y/n): ")
+    if confirm.lower() != 'y':
+        rich.print("Operation cancelled.")
+        return
+
+    # Check path
+    if not os.path.exists(args.path):
+        rich.print(format_log_error("Path does not exist."))
+
+    # Cycle files
+    result_array = []
+    for root, dirs, files in os.walk(args.path):
+        for file in files:
+            if file.endswith((".cpp", ".h", ".cs", ".ini")):
+                file_path = os.path.join(root, file)
+                result = convert_and_check_utf8(file_path, args.checks)
+                result_array.append(result)
+    rich.print("\n\n")
+
+
+    # print encoding before
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- common.py
+++ common.py
@@ -1,22 +1,27 @@
 import os
-import shutil
-import tempfile
-
+from dataclasses import dataclass
 import chardet
 import rich
-
 from util.checks import check_illegal_chars
 from util.iconv import convert_to_utf8_with_iconv
 from util.log import format_log_path, format_log_warning
 from util.path import copy_old_encoded_file
 
 
-def convert_and_check_utf8(path: str, checks: bool):
+@dataclass
+class ConvertResult:
+    status: bool
+    converted: bool
+    file_name: str
+    enc_before: str
+    enc_after: str
+    error_name: str | None = None
+
+
+def convert_and_check_utf8(path, checks: bool) -> ConvertResult:
 
     file_name = os.path.basename(path)
     target_encoding = "utf-8"
-
-
 
     try:
         rich.print("[purple]################################[/purple]")
@@ -45,14 +50,16 @@ def convert_and_check_utf8(path: str, checks: bool):
         if not is_already_utf8:
             check_illegal_chars(path, encoding, True) if checks else None
             convert_to_utf8_with_iconv(path, encoding, target_encoding)
+            return ConvertResult(status=True, converted=True, file_name=file_name, enc_before=encoding, enc_after=target_encoding)
         else:
             rich.print(format_log_warning(f"Skipping conversion of {file_name} because it is already UTF-8."))
             check_illegal_chars(path, encoding, False) if checks else None
+            return ConvertResult(status=True, converted=True, file_name=file_name, enc_before=encoding, enc_after=encoding)
 
 
     except RuntimeError as e:
         rich.print(f"\t[bold red]Conversion of {file_name} interrupted because of an error: {e}[/bold red]")
+        return ConvertResult(status=False, converted=False, file_name=file_name, error_name="RuntimeError", enc_before=encoding, enc_after=encoding)
     except InterruptedError as e:
         rich.print(format_log_warning(f"Conversion of {file_name} interrupted by user"))
-
-    rich.print("[purple]################################[/purple]")
+        return ConvertResult(status=False, converted=False, file_name=file_name, error_name="InterruptedError",  enc_before=encoding, enc_after=encoding)
