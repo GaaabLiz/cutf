445886875c66abd955b4bf8d84532bdaafb5b26c
Lizzos Gabriele <gabriele.lizzos@atom.it>
2025-01-03 16:18:18 +0100

    Removed old code

--- old2/model/MissingCharResult.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class MissingCharResult:
-    is_commented: bool
-    string: str
-    line: int
-    file_name: str
--- old2/util/log.py
+++
@@ -1,9 +0,0 @@
-
-def format_log_path(path: str):
-    return f"[bold magenta]{path}[/bold magenta]"
-
-def format_log_warning(string: str):
-    return f"[bold yellow]{string}[/bold yellow]"
-
-def format_log_error(string: str):
-    return f"[bold red]{string}[/bold red]"
