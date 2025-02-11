96acae2d9f808b9c804568b888bd5cee122e942c
Gabriele <lizzosgabriele@gmail.com>
2025-02-11 21:11:41 +0100

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.4.1'
+VERSION = '0.0.4.2'
 DESCRIPTION = 'Script to convert files to UTF-8.'
 LONG_DESCRIPTION = 'Script to convert text files from any encoding to UTF-8 (With BOM).'
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["chardet", "rich"],
+    install_requires=["chardet", "rich", "pylizlib"],
     entry_points={
         "console_scripts": [
             "cuft=cuft.app:main",
