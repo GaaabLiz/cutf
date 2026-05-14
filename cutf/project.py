afa3e9effbf089088840655e90b2489801061115
gabliz <lizzosgabriele@gmail.com>
2026-05-14 17:25:26 +0200

    feat: add project metadata in project.py

---
+++ cutf/project.py
@@ -0,0 +1,7 @@
+# fmt: off
+name = 'cutf'
+version = '0.0.8'
+description = 'CLI tool to scan and convert source files to UTF-8 with BOM.'
+requires_python = '>=3.10'
+authors = [('Gabliz', None)]
+# fmt: on
